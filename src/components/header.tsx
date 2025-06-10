638fba1b432a7e9fc7a6f975de54c8e35729228a
vinigs22 <viniciusgsimoni@gmail.com>
2025-06-09 21:13:25 -0300

    fix: image path

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,16 @@
+import Logo from '/logo.svg';
+import SuricatoImg from '/suricato.png';
+
 export function Header() {
   return (
     <header className="flex justify-between my-8">
-        <img src="src/assets/logo.svg" alt="Logo" />
-        <div className="flex gap-2 justify-center items-center">
-          <p className="hidden sm:text-gray sm:text-sm sm:block" >Welcome, <span className="text-primary font-bold">Suricato</span>!</p>
-          <img src="src/assets/suricato.png" className="size-12 rounded-full bg-gray-700"></img>
-        </div>
+      <img src={Logo} alt="Logo" />
+      <div className="flex gap-2 justify-center items-center">
+        <p className="hidden sm:text-gray sm:text-sm sm:block">
+          Welcome, <span className="text-primary font-bold">Suricato</span>!
+        </p>
+        <img src={SuricatoImg} className="size-12 rounded-full bg-gray-700" />
+      </div>
     </header>
-  )
+  );
 }
